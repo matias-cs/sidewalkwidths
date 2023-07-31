f2b76b09755c1ce5332c6a15e0a63f4a3dd7dd51
matias-cs <129774452+matias-cs@users.noreply.github.com>
2023-07-31 19:22:16 +0200

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,5 @@
 var MAPBOX_TOKEN = 'pk.eyJ1IjoibWF0aWFzY2FyZG9zbyIsImEiOiJjbGE1dGN1d3AweGpzM3ZvNzA2a3Z0ODMxIn0.9tjff8LSWQZoQ3O5CbSiOQ'
-var MAPBOX_STYLE = 'mapbox://styles/matiascardoso/clj1tc6m800dv01qq1i4ba1ki'
+var MAPBOX_STYLE = 'mapbox://styles/matiascardoso/clkoc58ab005s01o8ezb3d30t'
 var SIDEWALKS_TILESET = 'mapbox://matiascardoso.9gzno2y4'
 var SIDEWALKS_LAYER = 'Index_-_Amsterdamv3-5bbhxq'
 var DISTRICTS_TILESET = 'mapbox://dcharvey.7dbzv200'
