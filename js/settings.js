1066a0d53fee21d394cd79419791192db4680383
matias-cs <129774452+matias-cs@users.noreply.github.com>
2023-07-31 20:48:22 +0200

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,7 @@
 var MAPBOX_TOKEN = 'pk.eyJ1IjoibWF0aWFzY2FyZG9zbyIsImEiOiJjbGE1dGN1d3AweGpzM3ZvNzA2a3Z0ODMxIn0.9tjff8LSWQZoQ3O5CbSiOQ'
 var MAPBOX_STYLE = 'mapbox://styles/matiascardoso/clkoc58ab005s01o8ezb3d30t'
 var SIDEWALKS_TILESET = 'mapbox://matiascardoso.94x90g7n'
-var SIDEWALKS_LAYER = 'Walkability-cfbdg0'
+var SIDEWALKS_LAYER = 'Walkability2-9v7uqc'
 var DISTRICTS_TILESET = 'mapbox://dcharvey.7dbzv200'
 var DISTRICTS_LAYER = 'districts_nyc-1eoi4m'
 var UNITS = 'm' // change to 'm' for meters
