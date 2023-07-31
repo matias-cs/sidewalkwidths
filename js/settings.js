366367fd2a09e93867cccb54e0b59e64e2701ca6
matias-cs <129774452+matias-cs@users.noreply.github.com>
2023-07-31 20:43:30 +0200

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,6 @@
 var MAPBOX_TOKEN = 'pk.eyJ1IjoibWF0aWFzY2FyZG9zbyIsImEiOiJjbGE1dGN1d3AweGpzM3ZvNzA2a3Z0ODMxIn0.9tjff8LSWQZoQ3O5CbSiOQ'
 var MAPBOX_STYLE = 'mapbox://styles/matiascardoso/clkoc58ab005s01o8ezb3d30t'
-var SIDEWALKS_TILESET = 'mapbox://matiascardoso.5j5flxrf'
+var SIDEWALKS_TILESET = 'mapbox://matiascardoso.94x90g7n'
 var SIDEWALKS_LAYER = 'Walkability-cfbdg0'
 var DISTRICTS_TILESET = 'mapbox://dcharvey.7dbzv200'
 var DISTRICTS_LAYER = 'districts_nyc-1eoi4m'
@@ -8,37 +8,37 @@ var UNITS = 'm' // change to 'm' for meters
 var PRECISION = 0.1 // the number of decimal places
 var GROUPS = [
   {
-    "value": 0.0,
+    "value": 37,
     "rating": "impossible",
     "color": "#FF0049"
   },
   {
-    "value": 0.2,
+    "value": 42.3,
     "rating": "very difficult",
     "color": "#FF461E"
   },
   {
-    "value": 0.4,
+    "value": 47,
     "rating": "difficult",
     "color": "#FF9300"
   },
   {
-    "value": 0.6,
+    "value": 51.6,
     "rating": "somewhat difficult",
     "color": "#e4da27"
   },
   {
-    "value": 0.8,
+    "value": 56.9,
     "rating": "somewhat easy",
     "color": "#1ce262"
   },
   {
-    "value": 0.9,
+    "value": 63.9,
     "rating": "easy",
     "color": "#00FFC4"
   },
   {
-    "value": 1,
+    "value": 87.1,
     "rating": "very easy",
     "color": "#00D2FF"
   },
