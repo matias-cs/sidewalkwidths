db0c5e0f6f3c2e4fdbf7bd56bf4f7988fed495d6
matias-cs <129774452+matias-cs@users.noreply.github.com>
2023-08-01 13:48:55 +0200

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -9,37 +9,37 @@ var PRECISION = 0.1 // the number of decimal places
 var GROUPS = [
   {
     "value": 37,
-    "rating": "impossible",
+    "rating": "very bad",
     "color": "#FF0049"
   },
   {
     "value": 42.3,
-    "rating": "very difficult",
+    "rating": "bad",
     "color": "#FF461E"
   },
   {
     "value": 47,
-    "rating": "difficult",
+    "rating": "average",
     "color": "#FF9300"
   },
   {
     "value": 51.6,
-    "rating": "somewhat difficult",
+    "rating": "good",
     "color": "#e4da27"
   },
   {
     "value": 56.9,
-    "rating": "somewhat easy",
+    "rating": "good",
     "color": "#1ce262"
   },
   {
     "value": 63.9,
-    "rating": "easy",
+    "rating": "very good",
     "color": "#00FFC4"
   },
   {
     "value": 87.1,
-    "rating": "very easy",
+    "rating": "excellent",
     "color": "#00D2FF"
   },
 ]
